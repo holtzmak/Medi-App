cda2047629f0e6bc998f25063fd9a1ac856635c4
Kelly Holtzman <holtzmak@uregina.ca>
2020-11-22 20:51:51 -0600

    First draft of importing probability tables
    There will possibly be an issue with not having all 2^133 combinations in the conditionaly probability table...
    
    Co-Authored-By: Mansi Patel <44125953+mansiPaTeLL@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
+from symptom_and_disease_csv_util import get_symptom_probability_distribution
 from symptom_input_util import get_symptom_list_from_user_symptoms
+
+
+def get_symptom_names_from_symptom_probability_distribution():
+    symptom_probability_distribution = get_symptom_probability_distribution()
+    known_symptoms = []
+    for symptom, p1, not_symptom, p2 in symptom_probability_distribution:
+        known_symptoms.append(symptom)
+    return known_symptoms
 
 
 def get_symptoms_from_user():
@@ -6,15 +15,15 @@ def get_symptoms_from_user():
         'Please enter your symptoms separated by spaces (e.g. "headache itching fever"): '
     )
     user_symptoms = input()
-    # TODO: Get known_symptoms from csv file #8
+    known_symptoms = get_symptom_names_from_symptom_probability_distribution()
     user_entered_symptoms = get_symptom_list_from_user_symptoms(
-        user_symptoms=user_symptoms, known_symptoms=[]
+        user_symptoms, known_symptoms
     )
     while user_entered_symptoms is None:
         print("Please input your symptoms again")
         user_symptoms = input()
         user_entered_symptoms = get_symptom_list_from_user_symptoms(
-            user_symptoms=user_symptoms, known_symptoms=[]
+            user_symptoms, known_symptoms
         )
     return user_entered_symptoms
 
